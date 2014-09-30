90c77806fa0c3430819bc06fcd75177f23d3b1df
jordlidd <jrdnliddle@gmail.com>
2014-09-30 11:11:58 +0200

    Add sign in functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+
+  def create
+    user = User.where(email: params[:signin][:email]).first
+
+
+    if user && user.authenticate(params[:signin][:password])
+
+      session[:user_id] = user.id
+      flash[:notice] = "Sign in successful."
+
+      redirect_to root_url
+    else
+      flash[:error] = "Sorry. We couldn't sign you in."
+      render :new
+    end
+  end
+
+end
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -3,6 +3,8 @@ FactoryGirl.define do
     first_name "Bruce"
     last_name "Wayne"
     email "bruce@gotham.net"
+    password "secret"
+    password_confirmation "secret"
     city "Gotham"
   end
 end
